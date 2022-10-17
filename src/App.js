43e4a7ff0c8595e1282c41ee50e45b79012273ff
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-10-17 14:57:58 +1100

    structure directory

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { CategoryItem } from "./components";
 import "./categories.style.scss";
 const App = () => {
   const categories = [
@@ -30,14 +31,8 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div className="category-container" key={id}>
-          <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
