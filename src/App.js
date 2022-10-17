245996b75ea2289fc7c51a6be40fee0dfc0ab4e8
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-10-17 14:05:54 +1100

    drafing index page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+
+      {/* category 1 */}
+      <div className="category-container">
+        {/* <img src={}/> */}
+        <div className="category-container-body">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* category 2 */}
+      <div className="category-container">
+        {/* <img src={}/> */}
+        <div className="category-container-body">
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* category 3 */}
+      <div className="category-container">
+        {/* <img src={}/> */}
+        <div className="category-container-body">
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* category 4 */}
+      <div className="category-container">
+        {/* <img src={}/> */}
+        <div className="category-container-body">
+          <h2>Womens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* category 5 */}
+      <div className="category-container">
+        {/* <img src={}/> */}
+        <div className="category-container-body">
+          <h2>Mens</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
