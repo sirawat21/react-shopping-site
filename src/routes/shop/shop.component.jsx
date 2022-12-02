2173cadd68d1886528c143aa343f3e08d8cb86a2
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-12-02 16:21:05 +1100

    added shoping page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,14 @@
 import { useContext } from "react";
 import { ProductsContext } from "./../../contexts/product.context";
-
+import ProductCard from "./../../components/product-card/product-card.component"
+import "./shop.style.scss";
 const Shop = () => {
    const { products } = useContext(ProductsContext)
    return(
-      <div>
+      <div className="product-container">
          {
-            products.map(({name, id}) => (
-               <div key={ id }>
-                  <h1>{ name }</h1>
-               </div>
+            products.map((product) => (
+               <ProductCard key={product.id} product={product} />
             ))
          }
       </div>
