e7cfaa64d8137af50af9d7dd90e8ccf37aef274e
Brett Sira <brett@Bretts-MacBook-Pro.local>
2022-12-10 21:53:46 +1100

    update cart context handler

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,33 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 /* Helper cart added item */
 const addCartItem = (cartItems, productToAdd) => {
    const existingCartItem = cartItems.find(
       (cartItem) => cartItem.id === productToAdd.id
    );
-   if(existingCartItem) {
-      return cartItems.map((cartItem) => (cartItem.id === productToAdd.id) 
-         ? {...cartItem, quantity: cartItem.quantity + 1}
-         : cartItem
+   if (existingCartItem) {
+      return cartItems.map((cartItem) =>
+         cartItem.id === productToAdd.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
       );
    }
-   return [...cartItems, { ...productToAdd, quantity: 1}];
-}
+   return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
    isCartOpen: false,
    setIsCartOpen: () => {},
    cartItems: [],
    addItemToCart: () => {},
+   cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
    const [cartItems, setCartItems] = useState([]);
    const addItemToCart = (productToAdd) => {
       setCartItems(addCartItem(cartItems, productToAdd));
-   }
-
+   };
    const [isCartOpen, setIsCartOpen] = useState(false);
+   const [carthCount, setCartCount] = useState(0);
+
+   // watching when cartItem has changed then recount
+   useEffect(() => {
+      const newCartCount = cartItems.reduce(
+         (total, cartItem) => total + cartItem.quantity,
+         0
+      );
+      setCartCount(newCartCount);
+   }, [cartItems]);
+
    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
    return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
