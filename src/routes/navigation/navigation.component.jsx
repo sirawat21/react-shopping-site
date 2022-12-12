856107a5c2b78055cd7f865354db0e774ff215de
sirawat21 <sirawat21@gmail.com>
2022-12-12 14:32:29 +1100

    change site logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import ReactLogo from "../../assets/react-logo.png";
 import { UserContext } from "./../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "./../../components/cart-icon/cart-icon.component";
@@ -15,7 +15,7 @@ const Navigation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrwnLogo className="logo" />
+          <img src={ReactLogo} className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
